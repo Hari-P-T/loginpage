652035a0ebd09d66dda2b56e6dfc7c9c505a2d8d
Hari-P-T <112808592+Hari-P-T@users.noreply.github.com>
2023-10-18 12:54:25 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Button, StyleSheet } from 'react-native';
+
+const SignupPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (text) => {
+    setUsername(text);
+  };
+
+  const handlePasswordChange = (text) => {
+    setPassword(text);
+  };
+
+  const handleClear = () => {
+    setUsername('');
+    setPassword('');
+  };
+
+  const handleSubmit = () => {
+    // You can implement the submission logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  const handleForgotPassword = () => {
+    console.log('Forgot Password');
+    // You can implement password recovery logic here
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Signup Page</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={handleUsernameChange}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={handlePasswordChange}
+      />
+      <TouchableOpacity onPress={handleForgotPassword}>
+        <Text style={styles.forgotPassword}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <Button title="Clear" onPress={handleClear} />
+        <Button title="Submit" onPress={handleSubmit} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 20,
+  },
+  forgotPassword: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    marginBottom: 10,
+  },
+});
+
+export default SignupPage;
